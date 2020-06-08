e5409c30c59636b7ed1657645f07c35705883fba
unknown <lenalee.mendoza@gmail.com>
2020-06-08 14:02:20 +0800

    finalized but still subject to change/edit

--- loginscript.js
+++ loginscript.js
@@ -1,21 +1,64 @@
 function confirm(){
+    
+    /*sir, we have two login forms: one for the home page, one is modal.
+    sorry we don't know how to reuse html forms just yet ;-;
+    we had to use different codes for different id's in each login form
+    that's why the code became complicated ;-;*/
 
-    var uname = document.getElementById("uname").value; //remember to put id=uname in html
-    var pass = document.getElementById("pass").value; //remember to put id=pass in html
+    //this is for the home login
+    var uname = document.getElementById("uname").value;
+    var pass = document.getElementById("pass").value;
+
+    //this is for the modal login
+    var uname2 = document.getElementById("uname-2").value;
+    var pass2 = document.getElementById("pass-2").value;
+
     var text = "It works";
 
-    if(uname == "user" && pass == "user"){ //reminder this is below login form
-        document.getElementById("adminsuccess").innerHTML = ""; 
-        document.getElementById("usersuccess").innerHTML = text; //reminder to put id=usersuccess in html label loginform
-    }
+    if(uname != ""){
 
-    else if(uname == "admin" && pass == "admin"){ //reminder this is above login form
-        document.getElementById("usersuccess").innerHTML = ""; 
-        document.getElementById("adminsuccess").innerHTML = text;
+        if(uname == "user" && pass == "user"){
+            document.getElementById("adminsuccess").innerHTML = ""; 
+            document.getElementById("usersuccess").innerHTML = text;
+        }
+
+        else if(uname == "admin" && pass == "admin"){
+            document.getElementById("usersuccess").innerHTML = ""; 
+            document.getElementById("adminsuccess").innerHTML = text;
+        }
+
+        else{
+            document.getElementById("adminsuccess").innerHTML = ""; 
+            document.getElementById("usersuccess").innerHTML = ""; 
+            alert ("Credentials are incorrect");
+        }
+
     }
 
     else{
-        alert ("Credentials are incorrect");
+        if(uname2 == "user" && pass2 == "user"){
+            document.getElementById("adminsuccess-2").innerHTML = ""; 
+            document.getElementById("usersuccess-2").innerHTML = text;
+        }
+
+        else if(uname2 == "admin" && pass2 == "admin"){
+            document.getElementById("usersuccess-2").innerHTML = ""; 
+            document.getElementById("adminsuccess-2").innerHTML = text;
+        }
+
+        else{
+            document.getElementById("adminsuccess").innerHTML = ""; 
+            document.getElementById("usersuccess").innerHTML = "";
+            document.getElementById("adminsuccess-2").innerHTML = ""; 
+            document.getElementById("usersuccess-2").innerHTML = ""; 
+            alert ("Credentials are incorrect");
+        }
     }
+
+    document.getElementById("uname").value = "";
+    document.getElementById("pass").value = "";
+
+    document.getElementById("uname-2").value = "";
+    document.getElementById("pass-2").value = "";
 
 }
